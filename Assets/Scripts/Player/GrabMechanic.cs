92abf72cf7c62d2984ce5c20c28a70db7537bb82
DonMars <mars.virus@gmail.com>
2024-06-24 11:33:04 -0500

    P de Papá

---
+++ Assets/Scripts/Player/GrabMechanic.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabMechanic : MonoBehaviour
+{
+    public Transform grabPoint;
+    PlayerController player;
+    Grabbable grabbable;
+
+    float holdStartTime;
+
+    void Awake()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (player.canGrab && Input.GetKeyDown(player.grabKey))
+        {
+            if (grabbable == null) // If not carrying, try to grab
+            {
+                if (other.TryGetComponent(out grabbable))
+                {
+                    grabbable.Grab(grabPoint);
+                    player.isGrabbing = true;
+                    holdStartTime = Time.time;
+                }
+            }
+            else // If carrying, drop
+            {
+                grabbable.Drop();
+                grabbable = null;
+                player.isGrabbing = false;
+            }
+        }
+
+        if (player.canGrab && player.isGrabbing && Input.GetKeyUp(player.grabKey))
+        {
+            float holdDownTime = Time.time - holdStartTime;
+            grabbable.Throw();
+            grabbable = null;
+            player.isGrabbing = false;
+        }
+    }
+}
